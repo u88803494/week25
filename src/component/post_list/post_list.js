8192bd758437608fa50bd24c3bc0efef4c7de175
Hugh <u88803494@gmail.com>
2020-04-04 14:24:59 +0800

    板型修改中

--- src/component/post_list/post_list.js
+++ src/component/post_list/post_list.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-import { ListGroup, Button, Spinner, ButtonGroup, Media } from 'react-bootstrap';
+import { Button, Spinner, ButtonGroup } from 'react-bootstrap';
 import './post_list.css';
+import { ListPosts } from './ListPosts';
+import { GridPosts } from './GridPosts';
 
 /** 變動部分:
  * 1. 文章列表改版，變得更像 blog
@@ -10,77 +12,6 @@ import './post_list.css';
  * 4. 由一個 RenderPost 選擇要 render list 或是 blog
  * 5. 切割檔案出去
  */
-
-const RenderListPosts = ({ data, history, showManagementWindow }) => {
-  const handleShow = (e) => {
-    const { id, name } = e.target.dataset;
-    showManagementWindow({ method: name, postId: parseInt(id, 10) }); // event 接收的是 string
-  };
-
-  return (
-    <>
-      {data.map(post => (
-        <ListGroup.Item
-          key={post.id}
-          className="blog__post"
-        >
-          <div
-            className="blog__title"
-            onClick={() => history.push(`/posts/${post.id}`)}
-          >
-            {post.title}
-          </div>
-          <div className="blog__controller">
-            <Button
-              variant="outline-success"
-              data-name="editing"
-              data-id={post.id}
-              onClick={handleShow}
-            >
-              編輯
-            </Button>
-            <Button
-              variant="outline-danger"
-              data-name="delete"
-              data-id={post.id}
-              onClick={handleShow}
-            >
-              刪除
-            </Button>
-          </div>
-        </ListGroup.Item>
-      ))}
-    </>
-  );
-};
-
-const RenderGridPosts = ({ data, history, showManagementWindow }) => {
-  return (
-    <>
-      {data.map(post => ( // media 或是 jumbotron 修改
-        <Media className="blog__post blog__post--grid">
-          <img
-            width={64}
-            height={64}
-            className="mr-3"
-            src="holder.js/64x64"
-            alt="Generic placeholder"
-          />
-          <Media.Body>
-            <h5> {post.title} </h5>
-            <pre> {/* 限制高度，多的不顯示變成... */}
-              {post.body}
-            </pre>
-          
-          {/* 用點擊文章或標題看更多就好 */}
-          </Media.Body>
-        </Media>
-
-      ))}
-    </>
-  )
-}
-
 const PostsHeader = ({ isList, setIsList, handleShowWindows }) => {
   return (
     <header className="header">
@@ -110,6 +41,21 @@ const PostsHeader = ({ isList, setIsList, handleShowWindows }) => {
   )
 }
 
+const RenderPosts = ({ data, history, isList, showManagementWindow }) => {
+  const handleShow = (e) => {
+    const { id, name } = e.target.dataset;
+    showManagementWindow({ method: name, postId: parseInt(id, 10) }); // event 接收的是 string
+  };
+
+  return (
+    <>
+      {isList ? <ListPosts {...{ data, history, handleShow }} /> : <GridPosts {...{ data, history, handleShow }} />}
+    </>
+  )
+
+
+}
+
 const Posts = ({
   history, postsListData, showManagementWindow, getPosts, shouldGetPosts
 }) => {
@@ -127,7 +73,7 @@ const Posts = ({
       <main className="blog__posts">
         {/** 判斷是否讀取中 */
           postsListData.length
-            ? <RenderGridPosts data={postsListData} {...{ history, showManagementWindow, isList }} />
+            ? <RenderPosts data={postsListData} {...{ history, showManagementWindow, isList }} />
             : <Spinner animation="border" />
         }
       </main>
