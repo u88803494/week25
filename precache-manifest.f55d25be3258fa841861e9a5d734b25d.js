0827d1e1aeb1b58227f5b1c1298f1fc1e73f9d54
Hugh <u88803494@gmail.com>
2020-04-06 17:04:40 +0800

    Updates

---
+++ precache-manifest.f55d25be3258fa841861e9a5d734b25d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020c8184043eb64ca0955ea1ff7d5a93",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75797484915c7c69904b",
+    "url": "./static/css/main.32e6d937.chunk.css"
+  },
+  {
+    "revision": "356ebf9f045df90078fb",
+    "url": "./static/js/2.60e72609.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "./static/js/2.60e72609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75797484915c7c69904b",
+    "url": "./static/js/main.d17ca312.chunk.js"
+  },
+  {
+    "revision": "6b396b2a384f3a3b9472",
+    "url": "./static/js/runtime-main.6c8fc56f.js"
+  },
+  {
+    "revision": "bfb90f81a4d82b658b528331a92e0045",
+    "url": "./static/media/avator.bfb90f81.png"
+  },
+  {
+    "revision": "1014b352d693dfc3daf195bc5652fa84",
+    "url": "./static/media/image00.1014b352.jpg"
+  },
+  {
+    "revision": "dcf72a138d9e59760b5a49f78aedee8e",
+    "url": "./static/media/image01.dcf72a13.jpg"
+  },
+  {
+    "revision": "221979268f7a4389694b10fb2693f50c",
+    "url": "./static/media/image02.22197926.jpg"
+  },
+  {
+    "revision": "55971b6e9cb798bb8af8b09e30f108ad",
+    "url": "./static/media/image03.55971b6e.jpg"
+  },
+  {
+    "revision": "c0f37e5f8a177a788b5e227ed147b8f6",
+    "url": "./static/media/image04.c0f37e5f.jpg"
+  },
+  {
+    "revision": "7da8a1cd38e92aa6c1651cb5658f162f",
+    "url": "./static/media/image05.7da8a1cd.jpg"
+  },
+  {
+    "revision": "d5692a6dbcad5760063ab8b1db13870a",
+    "url": "./static/media/image06.d5692a6d.jpg"
+  },
+  {
+    "revision": "281f07055935876e334a9eb56e9ca218",
+    "url": "./static/media/image07.281f0705.png"
+  },
+  {
+    "revision": "20cd3467b7be33a76a609fad2eef44ca",
+    "url": "./static/media/image08.20cd3467.jpg"
+  },
+  {
+    "revision": "b9cde7cb5f94c8d920df1456ab721918",
+    "url": "./static/media/image09.b9cde7cb.jpg"
+  }
+]);
