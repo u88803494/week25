4f27080f5150e713c8b02155a93d408df96b5430
Hugh <u88803494@gmail.com>
2021-03-21 13:57:35 +0800

    Adjust the code style

--- src/component/post_list/grid_posts.js
+++ src/component/post_list/grid_posts.js
@@ -14,8 +14,18 @@ export const GridPosts = ({ data, historyPush, }) => (
     {data.map(post => (
       <Media className="blog__post blog__post--grid" key={post.id}>
         <Media.Body className="blog__body">
-          <h5 className="blog__title blog__title--grid" data-id={post.id} onClick={historyPush}> {post.title} </h5>
-          <p className="blog__content" data-id={post.id} onClick={historyPush}>
+          <h5
+            className="blog__title blog__title--grid"
+            data-id={post.id}
+            onClick={historyPush}
+          >
+            {post.title}
+          </h5>
+          <p
+            className="blog__content"
+            data-id={post.id}
+            onClick={historyPush}
+          >
             {post.body}
           </p>
           <div className="blog__author">
@@ -27,13 +37,21 @@ export const GridPosts = ({ data, historyPush, }) => (
               alt="Generic placeholder"
             />
             <div className="blog__author--info">
-              <div className="blog__author--ellipsis">{post.author}</div>
-              <small className="blog__author--ellipsis">{post.createdAt}</small>
+              <div className="blog__author--ellipsis">
+                {post.author}
+              </div>
+              <small className="blog__author--ellipsis">
+                {post.createdAt}
+              </small>
             </div>
           </div>
         </Media.Body>
         <Col xs={6} md={4} >
-          <Image className="blog__picture" src={imageData[parseInt(post.id % 10)]} thumbnail />
+          <Image
+            className="blog__picture"
+            src={imageData[parseInt(post.id % 10)]}
+            thumbnail
+          />
         </Col>
       </Media>
     ))}
